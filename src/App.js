3bfb2db315d8812f5c711c717866e9afa581a2d8
Marcin <mgabrys422@gmail.com>
2020-04-15 14:50:29 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import './Components/HamburgerButton';
+
+
 <div class="menu-btn">
   <div class="menu-btn__burger"></div>
 </div>
   <script src="assets/main.js"></script>
+
+export default App;
