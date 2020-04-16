65299d9ca84377a574ced6e75e729c80908728ea
alexhonigmann <alexhonigmann@gmail.com>
2020-04-16 12:48:12 +0200

    ver 005

--- src/Components/СontactForm.js
+++ src/Components/СontactForm.js
@@ -2,16 +2,16 @@ import React from 'react';
 import "./contactForm.css";
 
 const ContactForm = () => {
-    return ( 
+    return (
         <section>
             <div className="contact-box">
                 <div className="contact-text">
                     <h1>
-                        Hi, wie können wir dir helfen?
+                        Hi, wie können wir dir <span>helfen?</span>
                     </h1>
                     <p>An diesen Zeiten erreichst du uns am besten an folgenden Tagen und Zeiten</p>
                     <p>Montag - Freitag 09:00 - 17:00 Uhr</p>
-                    <p>Unser Campus befindet sich im Super7000 Coworcing Space -der Mutter aller Coworking Spaces. Die genau A dresse lautet:</p>
+                    <p>Unser Campus befindet sich im Super7000 Coworcing Space - der Mutter aller Coworking Spaces. Die genau Adresse lautet:</p>
                     <p>Ratherstr.25 40476 Düsseldorf</p>
                 </div>
                 <div className="contact-input">
@@ -20,11 +20,10 @@ const ContactForm = () => {
                     <input type="email" placeholder="EMAIL"></input>
                     <input type="number" placeholder="TELEFONNUMMER"></input>
                     <textarea placeholder="NACHRICHT"></textarea>
-                    <button>PLAY VIDEO</button>
                 </div>
             </div>
         </section>
-     );
+    );
 }
- 
+
 export default ContactForm;
